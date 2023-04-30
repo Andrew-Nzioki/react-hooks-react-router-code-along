49d11ce131da919f2753433e9b5c8cf6cd3ca74b
Andrew Kyalo Nzioki <andrewkyalonzioki001.com>
2023-04-30 21:47:27 +0300

    react router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// Step 1\. Import react-router functions
+import { BrowserRouter ,Routes, Route } from "react-router-dom"
 
+
+function About() {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
 function Home() {
   return (
     <div>
@@ -9,8 +36,14 @@ function Home() {
   );
 }
 
-function App() {
-  return <Home />;
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+// Step 2\. Change so router is coordinating what is displayed
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
